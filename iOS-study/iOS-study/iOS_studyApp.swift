7a148688c23dcfe9172623d78c0864d3ef6ec536
RyoTomiyoshi <110385079+ryoryo3939@users.noreply.github.com>
2023-06-20 16:21:34 +0900

    Add files via upload

---
+++ iOS-study/iOS-study/iOS_studyApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_studyApp.swift
+//  iOS-study
+//
+//  Created by S892 on 2023/06/20.
+//
+
+import SwiftUI
+
+@main
+struct iOS_studyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
