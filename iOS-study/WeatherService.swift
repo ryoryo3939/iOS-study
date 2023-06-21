cdbf824763d5d9c8eee1e00cca745112935b6b59
ryoryo3939 <ryo.tomiyoshi@asia-quest.jp>
2023-06-21 10:35:25 +0900

    session2

---
+++ iOS-study/WeatherService.swift
@@ -0,0 +1,20 @@
+//
+//  WeatherService.swift
+//  iOS-study
+//
+//  Created by S892 on 2023/06/21.
+//
+
+import Foundation
+import YumemiWeather
+
+class WeatherService {
+    static func fetchWeatherCondition(completion: @escaping (String?) -> Void) {
+        DispatchQueue.global(qos: .background).async {
+            let weather = YumemiWeather.fetchWeatherCondition()
+            DispatchQueue.main.async {
+                completion(weather)
+            }
+        }
+    }
+}
