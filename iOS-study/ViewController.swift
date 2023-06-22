d151f606efafe008ed505b92be0cd496baff8f89
ryoryo3939 <ryo.tomiyoshi@asia-quest.jp>
2023-06-22 10:48:21 +0900

    APIを叩く動作/天気の画像を実装

--- iOS-study/ViewController.swift
+++ iOS-study/ViewController.swift
@@ -13,7 +13,24 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    @IBOutlet weak var weatherImageView: UIImageView!
+    @IBOutlet weak var reloadButton: UIButton!
+    @IBAction func reloadButtonAction(_ sender: UIButton) {
+        WeatherService.fetchWeatherCondition { weather in
+            DispatchQueue.main.async {
+                switch weather {
+                case "sunny":
+                    self.weatherImageView.image = UIImage(named: "icon-sunny")
+                case "cloudy":
+                    self.weatherImageView.image = UIImage(named: "icon-cloudy")
+                case "rainy":
+                    self.weatherImageView.image = UIImage(named: "icon-rainy")
+                default:
+                    print("Failed to fetch weather.")
+                }
+            }
+        }
+    }
+    
 }
 
