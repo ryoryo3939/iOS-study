c3823b6080aa223d7aee26e13abdbe0b68222af0
ryoryo3939 <ryo.tomiyoshi@asia-quest.jp>
2023-07-03 16:17:14 +0900

    APIのエラーハンドリングを作成

--- iOS-study/WeatherViewController.swift
+++ iOS-study/WeatherViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import YumemiWeather
 
 class WeatherViewController: UIViewController, WeatherServiceDelegate {
     
@@ -29,9 +30,29 @@ class WeatherViewController: UIViewController, WeatherServiceDelegate {
         self.dismiss(animated: true, completion: nil)
     }
     
-    func didUpdateWeather(_ service: WeatherService, weaterType: WeatherType?) {
-                self.weatherImageView.image = weaterType?.image
+    func didUpdateWeather(_ service: WeatherService, weatherType: WeatherType?) {
+                self.weatherImageView.image = weatherType?.image
      }
+    
+    func didFailWithError(_ service: WeatherService, error: Error) {
+        let errorMessage = makeErrorMessage(from: error)
+        let alertController = UIAlertController(title: "Error", message: errorMessage, preferredStyle: .alert)
+        alertController.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        self.present(alertController, animated: true, completion: nil)
+    }
+    
+    func makeErrorMessage(from error: Error) -> String {
+        if let yumemiError = error as? YumemiWeatherError {
+            switch yumemiError {
+            case .invalidParameterError:
+                return "Invalid parameters!"
+            case .unknownError:
+                return "An unknown error occurred!"
+            }
+        } else {
+            return error.localizedDescription
+        }
+    }
     
     deinit {
         print("WeatherViewController deinitialized")
