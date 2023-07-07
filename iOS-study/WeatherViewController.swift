0e9674f3ead6a629ade86979a6d089f5b1a3216b
ryoryo3939 <ryo.tomiyoshi@asia-quest.jp>
2023-07-07 12:50:54 +0900

    既に引数として取得しているweatherTypeを再度取得していたので削除

--- iOS-study/WeatherViewController.swift
+++ iOS-study/WeatherViewController.swift
@@ -33,8 +33,7 @@ class WeatherViewController: UIViewController, WeatherServiceDelegate {
     }
     
     func didUpdateWeather(_ service: WeatherService, weatherResponse: WeatherResponse, weatherType: WeatherType) {
-        let weatherType = WeatherType(rawValue: weatherResponse.weatherCondition)
-        self.weatherImageView.image = weatherType?.image
+        self.weatherImageView.image = weatherType.image
         self.lowestTemperature.text = "\(weatherResponse.minTemperature)"
         self.highestTemperature.text = "\(weatherResponse.maxTemperature)"
      }
