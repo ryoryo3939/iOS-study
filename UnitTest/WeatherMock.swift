3164f2e89c8eb52a16ad06aa4e71a9adcaf8bc01
ryoryo3939 <ryo.tomiyoshi@asia-quest.jp>
2023-07-12 20:02:50 +0900

    モックを新たに作成

---
+++ UnitTest/WeatherMock.swift
@@ -0,0 +1,28 @@
+//
+//  WeatherMock.swift
+//  iOS-study
+//
+//  Created by S892 on 2023/07/11.
+//
+
+import Foundation
+
+class YumemiWeatherMock: WeatherFetchable {
+    var jsonStringToReturn: String?
+
+    func fetchWeather(_ jsonString: String) throws -> String {
+        return jsonStringToReturn ?? ""
+    }
+}
+
+class WeatherServiceMock: WeatherService {
+    var weatherResponse: WeatherResponse?
+    var weatherType: WeatherType?
+
+    override func fetchWeather() {
+        guard let weatherResponse = weatherResponse, let weatherType = weatherType else {
+            return
+        }
+        self.delegate?.didUpdateWeather(self, weatherResponse: weatherResponse, weatherType: weatherType)
+    }
+}
